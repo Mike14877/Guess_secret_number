7440b6a469ea1651f55635711abce0f0e1e382f8
WindSt <BraveNW148@gmail.com>
2020-12-09 21:17:29 -0500

    Initial commit

---
+++ guess_secret_number.py
@@ -0,0 +1,26 @@
+secret_number = 777
+print(
+    """
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+
+def guess_function():
+    guess_number = int(input("Guess a three int number : "))
+    right = False
+    while not right:
+        if guess_number != secret_number:
+            print("Ha ha! You're stuck in my loop!")
+            return guess_function()
+        else:
+            right = True
+            return "Well done, muggle! You are free now"
+
+
+guess_function()
